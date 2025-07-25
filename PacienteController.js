5cc34810f4b490736309f3a629cdeb8e8366f10d
Rensso26 <135520250+Rensso26@users.noreply.github.com>
2025-07-24 19:52:01 -0500

    Add files via upload

--- PacienteController.js
+++ PacienteController.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const URI_BASE="http://localhost:8081/api/examen/v1/pacientes"
 
 const guardar = async (body) =>{
-    axios.post(`${URI_BASE}`,body).then(r =>r.data);
+    axios.put(`${URI_BASE}`,body).then(r =>r.data);
 }
 
 export const guardarFachada = async (body) =>{
